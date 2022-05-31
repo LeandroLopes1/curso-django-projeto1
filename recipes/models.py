01bcbf8f8af8629c872a7b37a01241780e97e444
Leandro Lopes <leandro_carlos@uol.com.br>
2022-05-31 17:15:25 -0300

    criando as tabelas no banco de dados

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,9 @@
-from venv import create
+from django.contrib.auth.models import User
 from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -15,6 +19,6 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d')
-    
-    def __str__(self):
-        return self.title
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
